bae3b9c8e1032b10c7b5517a370674a2478b266a
Tushar Maliwal <tusharmaliwal2001@gmail.com>
2023-11-23 11:15:00 +0530

    added skeleton code in customer client

--- GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/business/CustomerService.java
+++ GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/business/CustomerService.java
@@ -1,32 +1,56 @@
 package com.flipkart.business;
 
 import com.flipkart.bean.Customer;
+import com.flipkart.bean.Slots;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerService {
-    public Object getAllGymCenterDetails(){
+    public List<Object> getAllGymCenterDetails(){
         //takes City (Location) as input and returns List<GymCenter>
-        return new Object();
+        System.out.println("Gym center details successfully sent");
+        return new ArrayList<Object>();
     }
 
-    public Object getAvailableSlots(){
+    public List<Object> getAvailableSlots(){
         //takes centerID and date for input and returns List<Slot>
-        return new Object();
+        System.out.println("Available slots successfully sent");
+        return new ArrayList<Object>();
     }
 
-    public Object getUserBookings(){
+    public List<Object> getCustomerBookings(){
         //takes userId and returns List<Bookings>
+        System.out.println("customer details successfully sent");
+        return new ArrayList<Object>();
+    }
+
+    public Object getBookingDetailsByID(){
+        System.out.println("booking details successfully sent");
         return new Object();
     }
 
     public Object register(){
         //takes customer Object and returns true and redirects to customer home
+        System.out.println("customer registered successfully");
         return new Object();
     }
 
     public Object login(){
         //takes username and password, verifies role and redirects to customer home
+        System.out.println("customer logged in successfully");
         return new Object();
     }
 
+    public Object bookSlot(){
+        //book a slot if available
+        System.out.println("slot booked successfully");
+        return new Object();
+    }
 
+    public Object cancelBookingbyID(){
+        //cancel a booking
+        System.out.println("cancelled booking successfully");
+        return new Object();
+    }
 }
--- GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/client/CustomerClient.java
+++ GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/client/CustomerClient.java
@@ -1,7 +1,5 @@
 package com.flipkart.client;
 
-import com.flipkart.bean.Customer;
-import com.flipkart.bean.GymCentre;
 import com.flipkart.business.CustomerService;
 
 import java.util.List;
@@ -12,17 +10,27 @@ public class CustomerClient {
 
         CustomerService customerService  =  new CustomerService();
 
-        Customer customer   = (Customer) customerService.register();
+        customerService.register();
 
-        System.out.println("Get Gym Details");
-        List<GymCentre> ListgymCentre = (List<GymCentre>) customerService.getAllGymCenterDetails();
+        System.out.println("Get List of Gym Centres for a location -------");
+        List<Object> gymCentreList = customerService.getAllGymCenterDetails();
 
-        System.out.println("Get All available slots");
-        List<Slot> Slot = (List<Slot>) customerService.getAvailableSlots();
+        System.out.println("Get All available slots -------");
+        List<Object> slotList = customerService.getAvailableSlots();
 
+        System.out.println("Get User Bookings Details -------");
+        List<Object> bookingList = customerService.getCustomerBookings();
 
+        System.out.println("Get Specific booking details -------");
+        customerService.getBookingDetailsByID();
 
+        System.out.println("Book a Slot -------");
+        customerService.bookSlot();
 
+        System.out.println("Cancel Booking -------");
+        customerService.cancelBookingbyID();
+
+        System.out.println("Get Centre Details -------");
         customerService.getAllGymCenterDetails();
     }
 
