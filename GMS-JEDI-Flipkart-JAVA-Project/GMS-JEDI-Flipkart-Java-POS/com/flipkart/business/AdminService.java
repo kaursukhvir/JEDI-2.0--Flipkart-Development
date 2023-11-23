1d5916eafad310d84d5ba01b27bcb60010130825
Apoorv <apoorv852@gmail.com>
2023-11-23 11:05:17 +0530

    Added admin client

--- GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/business/AdminService.java
+++ GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/business/AdminService.java
@@ -3,26 +3,31 @@ package com.flipkart.business;
 public class AdminService {
     public Object approveGymCenter(){
         //takes GymCenter Object as input and return boolean
+        System.out.println("Approved/Rejected Gym Center: ");
         return new Object();
     }
 
     public Object approveGymOwner(){
         //takes GymOwner Object as input and return boolean
+        System.out.println("Approved/Rejected Gym Owner: ");
         return new Object();
     }
 
     public Object viewPendingGymCenters(){
         //views all pending requests
+        System.out.println("Viewing pending Gym Center Approvals: ");
         return new Object();
     }
 
     public Object viewPendingGymOwners(){
         //views all pending requests
+        System.out.println("Viewing pending Gym Owner Approvals: ");
         return new Object();
     }
 
     public Object login(){
         //takes username and password, verifies role and redirects to admin home
+        System.out.println("Successful login for Admin");
         return new Object();
     }
 }
