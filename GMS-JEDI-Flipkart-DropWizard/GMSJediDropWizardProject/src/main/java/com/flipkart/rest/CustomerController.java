038e6e073b21390bc3ef542c02eeed65d3456347
kaursukhvir <kaursukh61324@gmail.com>
2023-11-28 23:40:13 +0530

    added viewMyProfile

--- GMS-JEDI-Flipkart-DropWizard/GMSJediDropWizardProject/src/main/java/com/flipkart/rest/CustomerController.java
+++ GMS-JEDI-Flipkart-DropWizard/GMSJediDropWizardProject/src/main/java/com/flipkart/rest/CustomerController.java
@@ -1,24 +1,48 @@
 package com.flipkart.rest;
 
 import com.flipkart.business.CustomerService;
-import com.flipkart.client.CustomerClient;
+
+import com.flipkart.bean.Customer;
+import com.flipkart.business.CustomerService;
+import com.flipkart.business.CustomerServiceInterface;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/customer")
 @Produces(MediaType.APPLICATION_JSON)
 public class CustomerController {
-    CustomerService customerService = new CustomerService();
+    CustomerServiceInterface customerService = new CustomerService();
+
+    //Customer login
     @GET
     @Path("/{userName}/{password}")
-    public String customerLogin(@PathParam("userName") String userName, @PathParam("password") String password) {
-
-        if(customerService.isUserValid(userName, password)){
-            return "Login Successful";
+    public Response customerLogin(@PathParam("userName") String userName, @PathParam("password") String password) {
+        try{
+            if (customerService.isUserValid(userName, password)) {
+                System.out.println("Login Successful");
+                return getCustomerProfile(userName);
+            } else {
+                System.out.println("Login Failed for " + userName);
+                return Response.status(Response.Status.UNAUTHORIZED).build();
+            }
+        }catch (Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
         }
-        else{
-            return "Login Failed "+userName+password;
+
+    }
+
+    //View profile
+    @GET
+    @Path("/viewprofile/{userName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCustomerProfile(@PathParam("userName") String userName){
+        Customer customerProfile = customerService.viewMyProfile(userName);
+        try{
+            return Response.ok(customerProfile).build();
+        }catch (Exception exception){
+            return Response.status(Response.Status.UNAUTHORIZED).entity(exception.getMessage()).build();
         }
     }
 
